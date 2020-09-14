b288e801d8cc22c346f5a78edb07a93a8182bf37
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-09-14 15:22:23 +0200

    init commit

---
+++ app/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function ExampleComponent() {
+  return (
+    <div>
+      <h1>This is our app!!!!!!!!!!</h1>
+      <p>The sky is blue hell yeah</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<ExampleComponent />, document.querySelector('#app'));
+
+/* refresh the page asynchronously */
+if (module.hot) {
+  module.hot.accept();
+}
