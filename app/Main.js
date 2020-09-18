7421c97c41c059803c00a1b2278ff19ae8ac8045
CodeIsaMystic <julienorjollet.wd@gmail.com>
2020-09-18 15:53:19 +0200

    setting up chat app

--- app/Main.js
+++ app/Main.js
@@ -1,4 +1,4 @@
-import React, { useState , useReducer, useEffect } from 'react';
+import React, { useState, useReducer, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { useImmerReducer } from 'use-immer';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
@@ -25,39 +25,44 @@ import Profile from './components/Profile';
 import EditPost from './components/EditPost';
 import NotFound from './components/NotFound';
 import Search from './components/Search';
-
-
+import Chat from './components/Chat';
 
 function Main() {
-  
   const initialState = {
-    loggedIn: Boolean(localStorage.getItem("complexappToken")),
+    loggedIn: Boolean(localStorage.getItem('complexappToken')),
     flashMessages: [],
     user: {
-      token: localStorage.getItem("complexappToken"),
-      username:  localStorage.getItem("complexappUsername"),
-      avatar:  localStorage.getItem("complexappAvatar")
+      token: localStorage.getItem('complexappToken'),
+      username: localStorage.getItem('complexappUsername'),
+      avatar: localStorage.getItem('complexappAvatar'),
     },
-    isSearchOpen: false
+    isSearchOpen: false,
+    isChatOpen: false,
   };
 
   function ourReducer(draft, action) {
     switch (action.type) {
-      case "login": 
+      case 'login':
         draft.loggedIn = true;
         draft.user = action.data;
         return;
-      case "logout":
+      case 'logout':
         draft.loggedIn = false;
         return;
-      case "flashMessage": 
+      case 'flashMessage':
         draft.flashMessages.push(action.value);
         return;
-      case "openSearch": 
+      case 'openSearch':
         draft.isSearchOpen = true;
         return;
-      case "closeSearch": 
+      case 'closeSearch':
         draft.isSearchOpen = false;
+        return;
+      case 'toggleChat':
+        draft.isChatOpen = !draft.isChatOpen;
+        return;
+      case 'closeChat':
+        draft.isChatOpen = false;
         return;
     }
   }
@@ -65,55 +70,54 @@ function Main() {
   const [state, dispatch] = useImmerReducer(ourReducer, initialState);
 
   useEffect(() => {
-    if(state.loggedIn) {
-      localStorage.setItem("complexappToken", state.user.token);
-      localStorage.setItem("complexappUsername", state.user.username);
-      localStorage.setItem("complexappAvatar", state.user.avatar);
+    if (state.loggedIn) {
+      localStorage.setItem('complexappToken', state.user.token);
+      localStorage.setItem('complexappUsername', state.user.username);
+      localStorage.setItem('complexappAvatar', state.user.avatar);
     } else {
-      localStorage.removeItem("complexappToken");
-      localStorage.removeItem("complexappUsername");
-      localStorage.removeItem("complexappAvatar");
+      localStorage.removeItem('complexappToken');
+      localStorage.removeItem('complexappUsername');
+      localStorage.removeItem('complexappAvatar');
     }
   }, [state.loggedIn]);
 
-
-
   return (
     <StateContext.Provider value={state}>
-      <DispatchContext.Provider value={dispatch}> 
+      <DispatchContext.Provider value={dispatch}>
         <BrowserRouter>
           <FlashMessages messages={state.flashMessages} />
           <Header />
           <Switch>
-            <Route path="/profile/:username">
+            <Route path='/profile/:username'>
               <Profile />
             </Route>
-            <Route path="/" exact>
+            <Route path='/' exact>
               {state.loggedIn ? <Home /> : <HomeGuest />}
             </Route>
-            <Route path="/post/:id" exact>
+            <Route path='/post/:id' exact>
               <ViewSinglePost />
             </Route>
-            <Route path="/post/:id/edit" exact>
+            <Route path='/post/:id/edit' exact>
               <EditPost />
             </Route>
-            <Route path="/create-post">
+            <Route path='/create-post'>
               <CreatePost />
             </Route>
-            <Route path="/about-us">
+            <Route path='/about-us'>
               <About />
             </Route>
-            <Route path="/terms">
+            <Route path='/terms'>
               <Terms />
             </Route>
             <Route>
               <NotFound />
             </Route>
-            </Switch>
-            <CSSTransition timeout={330} in={state.isSearchOpen} classNames="search-overlay" unmountOnExit>
-              <Search />
-            </CSSTransition>
-            <Footer />
+          </Switch>
+          <CSSTransition timeout={330} in={state.isSearchOpen} classNames='search-overlay' unmountOnExit>
+            <Search />
+          </CSSTransition>
+          <Chat />
+          <Footer />
         </BrowserRouter>
       </DispatchContext.Provider>
     </StateContext.Provider>
